2f5fead4095e0ccc0a6fa07d673e2cbe01586243
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-18 15:18:06 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // تصحيح الرابط هنا ليكون master-SNAPSHOT
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -37,32 +36,23 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // تأكد من اسم المستودع الخاص بك هنا
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "youssefsouri90-art/carateen-extension")
     }
 
     android {
         namespace = "com.momen.carateen"
         compileSdkVersion(35)
-
         defaultConfig {
             minSdk = 21
             targetSdk = 35
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
             }
         }
     }
@@ -71,9 +61,7 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
-        // تصحيح: استخدام master-SNAPSHOT لضمان وجود Requests و app
         cloudstream("com.github.lagradost:cloudstream3:master-SNAPSHOT")
-        
         implementation(kotlin("stdlib"))
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
