685cd9d2c61d085769e14cb2e607b8b587994b76
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-18 10:36:29 +0100

    Create build.gradle.kts

---
+++ CarateenProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "ar"
+    authors = listOf("Momen")
+    status = 1
+    tvTypes = listOf("TvSeries", "Anime", "Movie", "Cartoon")
+    iconUrl = "https://carateen.tv/wp-content/uploads/2024/08/favicon.png"
+    description = "Carateen provider built from page scraping instead of the older episode API."
+}
