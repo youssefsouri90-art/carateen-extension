7798dd2f797450fcee56ad48f1eb67bbe0367092
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-18 15:47:17 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,79 +1,14 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) =
-    extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "youssefsouri90-art/carateen-extension")
-    }
-
-    android {
-        namespace = "com.momen.carateen"
-        compileSdkVersion(35)
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 35
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_17)
-                freeCompilerArgs.addAll(
-                    "-Xskip-metadata-version-check",
-                    "-Xallow-unstable-dependencies"
-                )
-            }
-        }
-    }
-
-    dependencies {
+dependencies {
         val cloudstream by configurations
         val implementation by configurations
 
-        // الاعتماد على نسخة pre-release لأنها تحتوي على كل التبعيات مدمجة
-        cloudstream("com.github.lagradost:cloudstream3:pre-release")
+        // استيراد النسخة التي تحتوي بالتأكيد على NiceHttp مدمجة أو كاعتمادية واضحة
+        cloudstream("com.github.lagradost:cloudstream3:master-SNAPSHOT")
+        
+        // استيراد NiceHttp بشكل منفصل ولكن برابط JitPack الصحيح (حالة الأحرف مهمة)
+        implementation("com.github.lagradost:NiceHttp:main-SNAPSHOT")
         
         implementation(kotlin("stdlib"))
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
     }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
