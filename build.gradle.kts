08a595bee3afbf8e3cd75a074b6308a0b64afc3d
youssefsouri90-art <youssefsouri90@gmail.com>
2026-04-18 15:51:38 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,75 @@
-dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
-        // استيراد النسخة التي تحتوي بالتأكيد على NiceHttp مدمجة أو كاعتمادية واضحة
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    // تعريف الـ Configurations يدوياً لحل مشكلة "not found"
+    val cloudstream by configurations.creating
+    val implementation by configurations.getting
+
+    extensions.configure<CloudstreamExtension> {
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "youssefsouri90-art/carateen-extension")
+    }
+
+    extensions.configure<BaseExtension> {
+        namespace = "com.momen.carateen"
+        compileSdkVersion(35)
+
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 35
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+
+        tasks.withType<KotlinJvmCompile> {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_17)
+                freeCompilerArgs.addAll("-Xskip-metadata-version-check")
+            }
+        }
+    }
+
+    dependencies {
+        // سحب المكتبات بالروابط المباشرة الصحيحة
         cloudstream("com.github.lagradost:cloudstream3:master-SNAPSHOT")
-        
-        // استيراد NiceHttp بشكل منفصل ولكن برابط JitPack الصحيح (حالة الأحرف مهمة)
         implementation("com.github.lagradost:NiceHttp:main-SNAPSHOT")
         
         implementation(kotlin("stdlib"))
         implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
     }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
